771016e2176180946792ca313b999e3fdc9fadaa
danpinap <danny.pina.est@tecazuay.edu.ec>
2023-03-28 08:36:31 -0500

    v1

---
+++ operacionesbásicas.py
@@ -0,0 +1,12 @@
+dato1=int(input("digite el primer dígito: "))
+dato2=int(input("digite el segundo dígito: "))
+dato3=int(input("digite el tercer dígito: "))
+suma=(dato1+dato2+dato3)
+print("El resultado de la suma es: ", suma)
+resta=(dato1-dato2)
+print("El resultado de la resta es: ", resta)
+multiplicación=(dato1*dato2*dato3)
+print("El resultado de la multiplicación es: ", multiplicación)
+division=(dato1/dato2)
+print("El resultado de la división es: ", division)
+45
