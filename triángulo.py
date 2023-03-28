cad06dc058cbde236bab9bfc79d68ba8128ed70f
danpinap <danny.pina.est@tecazuay.edu.ec>
2023-03-28 08:18:49 -0500

    v2

--- triángulo.py
+++ triángulo.py
@@ -1,5 +1,4 @@
-base=int(input("Ingrese el valor de la base: "))
-altura=int(input("Ingrese el valor dela altura: "))
+base=float(input("Ingrese el valor de la base: "))
+altura=float(input("Ingrese el valor dela altura: "))
 área=(base*altura)/2
 print("El área del triángulo es:", área)
-10
