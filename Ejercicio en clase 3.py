14eb7e7530fe4829c42437f254e79eaecb61c79c
danpinap <danny.pina.est@tecazuay.edu.ec>
2023-03-29 18:00:32 -0500

    v1

---
+++ Ejercicio en clase 3.py
@@ -0,0 +1,9 @@
+nombre=input('Ingrese su nombre completo: \n')
+#Opción uno de salida de información.
+print('¡Hola ' + nombre + ' ! \n')
+#Opción dos de salida de información.
+print(f'¡Hola {nombre} !\n')
+#Opción tres de salida de información.
+print('¡Hola {} !\n'.format(nombre))
+#Opción cuatro de salida de información.
+print('¡Hola', nombre, '!' '\n')
