15217d4af337b5abe318d8a4bafdaef367eb7ae7
danpinap <danny.pina.est@tecazuay.edu.ec>
2023-03-29 07:23:54 -0500

    v1

---
+++ Ejercicio en clase 1.py
@@ -0,0 +1,11 @@
+nombre= "Danny Adrián"
+apellido= "Piña Piña"
+print(nombre, apellido)
+print(nombre+" "+apellido)
+print(nombre*5)
+print(len(nombre))
+#Conversion de datos de texto
+número=int(input("Ingrese número"))
+print(type(número))
+cadena=str(número)
+print(type(cadena))
