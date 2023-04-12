7eacbbb03c4d5a5a330cb5e7640620cd696498d4
danpinap <danny.pina.est@tecazuay.edu.ec>
2023-04-12 09:28:52 -0500

    v-1

--- módulos/Ejecutable.py
+++ módulos/Ejecutable.py
@@ -1,4 +1,5 @@
 from funciones import*
+from paquete.metodo import*
 #####MENÚ DE OPCIONES#####
 opcion=0
 while opcion>=0 and opcion>=10:
@@ -7,6 +8,7 @@ while opcion>=0 and opcion>=10:
     print('2.Resta')
     print('3.Multiplicación')
     print('4.División')
+    print('5.Saludar')
     print('0.Salir')
     opcion=int(input('Ingrese una opción: '))
     if opcion==0:
@@ -16,3 +18,19 @@ while opcion>=0 and opcion>=10:
         a=int(input('Ingrese el primer número: '))
         b=int(input('Ingrese el segundo número_: '))
         print('El resultado de la suma es: ',suma(a,b))
+    elif opcion==2:
+        a=int(input('Ingrese el primer número: '))
+        b=int(input('Ingrese el segundo número_: '))
+        print('El resultado de la resta es: ',resta(a,b))
+    elif opcion==3:
+        a=int(input('Ingrese el primer número: '))
+        b=int(input('Ingrese el segundo número_: '))
+        print('El resultado de la multiplicación es: ',multiplicacion(a,b))
+    elif opcion==4:
+        a=int(input('Ingrese el primer número: '))
+        b=int(input('Ingrese el segundo número_: '))
+        print('El resultado de la división es: ',division(a,b))
+    elif opcion==4:
+        print('Adrian', saludar())
+    else:
+        print('Opción ingresada incorrecta')
